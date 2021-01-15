8422c1b854d10aa5435303a7d1f0529ba3f1daa7
n396146-mora <74068106+n396146-mora@users.noreply.github.com>
2021-01-15 12:07:15 -0500

    finished

--- doThis.py
+++ doThis.py
@@ -1,3 +1,21 @@
 #imports:
-
+import random
 #code:
+print("Hello user were going to play a guessing game")
+low=int(input("Enter the lowest number you want me to choose from: "))
+high=int(input("Enter the highest number you want me to choose from: "))
+num=0
+print("i've chosen my number from " + str(low) + "-" + str(high))
+num=random.randint(low,high)
+guess=0
+tries=0
+while(num!=guess):
+	guess=int(input("Enter your guess: "))
+	tries=tries+1
+	if(guess>num):
+		print("Try lower")
+	else:
+		print("Try higher")
+if(num==guess):
+	print("You got it!")
+	print("it  took you " + str(tries) + " tries to guess the number.")
